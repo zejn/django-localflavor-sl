c18ed8cc8eb0c20d9ccaea55bf626a60b8997c5e
Domen Kožar <domen@dev.si>
2010-12-25 14:05:24 +0100

    PEP8ify and improve documentation

--- localflavor/sl/forms.py
+++ localflavor/sl/forms.py
@@ -11,15 +11,16 @@ from sl_postalcodes import SL_POSTALCODES
 sorted_sl_postalcodes = sorted(SL_POSTALCODES, key=lambda k: k[1])
 
 class EMSOField(CharField):
+    """A form for validating Slovenian personal identification number.
+
+    Additionally stores gender and birthday to self.info dictionary.
     """
-    A form for validating Slovenian personal identification number.
-    """
-    
+
     default_error_messages = {
         'invalid': _(u'Enter a valid personal identification number.'),
     }
     emso_regex = re.compile('^([0-3][0-9])([01][0-9])(\d{3})(\d{2})(\d{3})(\d)$')
-    
+
     def clean(self, value):
         value = value.strip()
         if len(value) != 13:
@@ -28,31 +29,32 @@ class EMSOField(CharField):
         m  = self.emso_regex.match(value)
         if m is None:
             raise ValidationError(self.default_error_messages['invalid'])
-        
+
+        # validation
         s = 0
         int_values = [int(i) for i in value]
-        for a,b in zip(int_values, range(7,1,-1)*2):
-            s += a*b
+        for a, b in zip(int_values, range(7, 1, -1) * 2):
+            s += a * b
         chk = s % 11
         if chk == 10:
             raise ValidationError(self.default_error_messages['invalid'])
         if int_values[-1] != 11 - chk:
             raise ValidationError(self.default_error_messages['invalid'])
-        
+
+        # extract info
         d = [int(i) for i in m.groups()]
         year = d[2] + 1000
         if year < 1700:
             year += 1000
         birthdate = datetime.date(year, d[1], d[0])
-        
         gender = d[4] < 500 and 'male' or 'female'
-        
+
         self.info = {'gender': gender, 'birthdate': birthdate}
         return value
 
 class SLTaxNumber(CharField):
     default_error_messages = {
-        'invalid': _(u'Enter a valid Slovenian tax number.'),
+        'invalid': _(u'Enter a valid tax number in form SIXXXXXXXX'),
     }
     sitax_regex = re.compile('^[1-9]\d{6}\d$')
     
@@ -73,7 +75,7 @@ class SLTaxNumber(CharField):
             raise ValidationError(self.default_error_messages['invalid'])
         if chk == 10:
             chk = 0
-        
+
         if int_values[-1] != chk:
             raise ValidationError(self.default_error_messages['invalid'])
 
